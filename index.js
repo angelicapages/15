96b3497f98ac571177b08c1a60ae4089ac4bfa2f
angelicapages <angelicapages@gmail.com>
2020-08-19 21:51:08 -0300

    dos ejercicios más

--- index.js
+++ index.js
@@ -39,3 +39,31 @@ const avanzarSemaforo = (colorAcutal) => {
 alert (avanzarSemaforo ("verde"))
 alert (avanzarSemaforo ("rojo"))
 alert (avanzarSemaforo ("amarillo"))
+
+//EJERCICIO 3
+
+const esConsonante = (letra) => {
+    if (letra !== letra =="a" || letra== "e" || letra== "i" || letra== "o" || letra== "u") {
+    return "no es consonante"
+    }
+    else {
+        return "Es una consonante"
+    }
+}
+alert (esConsonante ("o"))
+alert (esConsonante("p"))
+alert (esConsonante("i"))
+//EJERCICIO 4
+
+const puedeRenovarCarnet = (pasoTest, tieneMultasImpagas, pagoImpuestos) => {
+    if (pasoTest== true && tieneMultasImpagas== false && pagoImpuestos== true) {
+        return "Usted puede revalidar su carnet"
+    }
+    
+    else {
+        return "Usted no puede revalidar su carnet" 
+    }
+}
+    alert (puedeRenovarCarnet (true, false, false))
+    alert (puedeRenovarCarnet (false,true, false))
+    alert (puedeRenovarCarnet (true, false, true))
